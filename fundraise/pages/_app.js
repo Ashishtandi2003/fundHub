03683bbdf74e737cc80107670e7c8accacee881e
Akaza <ashishtandi820@gmail.com>
2025-04-02 09:52:58 +0530

    font change problem when deploy

--- fundraise/pages/_app.js
+++ fundraise/pages/_app.js
@@ -1,12 +1,22 @@
 import Layout from '../components/layout/Layout';
 import '../style/global.css';
+import { Roboto } from 'next/font/google';
+
+// Load Roboto font
+const roboto = Roboto({
+  weight: ['400', '700'], // Add required font weights
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 function MyApp({ Component, pageProps }) {
-  return(
-  <Layout> 
-  <Component {...pageProps} />;
-  </Layout>
-)
+  return (
+    <main className={roboto.className}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
